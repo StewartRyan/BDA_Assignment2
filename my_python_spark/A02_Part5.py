49a48dfb1949900b5c32bfc42e013d092cbe2af5
Ryan Stewart <ryan.stewart@mycit.ie>
2020-05-05 21:16:23 +0100

    Final commit

--- my_python_spark/A02_Part5.py
+++ my_python_spark/A02_Part5.py
@@ -1,3 +1,6 @@
+# Additional imports required for get_key_value()
+import ast
+
 # ------------------------------------------
 # FUNCTION my_map
 # ------------------------------------------
@@ -24,6 +27,31 @@ def my_map(my_input_stream, my_output_stream, my_mapper_input_parameters):
         my_output_stream.write(key + "\t" + str(value) + "\n")
 
 
+# ---------------------------------------
+#  FUNCTION get_key_value
+# ---------------------------------------
+def get_key_value(line):
+    # 1. We create the output variable
+    res = ()
+
+    # 2. We remove the end of line char
+    line = line.replace('\n', '')
+
+    # 3. We get the key and value
+    words = line.split('\t')
+    title = words[0]
+    value = words[1]
+
+    # 4. We process the value into a valid tuple type
+    value = ast.literal_eval(value)
+
+    # 4. We assign res
+    res = (title, value)
+
+    # 5. We return res
+    return res
+
+
 # ------------------------------------------
 # FUNCTION my_reduce
 # ------------------------------------------
@@ -32,37 +60,30 @@ def my_reduce(my_input_stream, my_output_stream, my_reducer_input_parameters):
 
     for item in my_input_stream:
         # Process line
-        # NOTE: get_key_value() returns a tuple with a key and value
-        # where the value is another tuple: e.g: ('WikiVoyage', 6, 'Dutch')
         processed_entry = get_key_value(item)
 
-        # Extrapolate web page name
-        web_page_name = processed_entry[0]
+        title = processed_entry[0]
         others = processed_entry[1]
 
-        # Extrapolate the relevant delimited values
         wikimedia_project = others[0]
         num_views = int(others[1])
         language = others[2]
 
-        # Make a key by which we want to determine our results
         key = wikimedia_project + '_' + language
 
-        # Set a default for if there is no entry for given key
-        default = (web_page_name, num_views)
+        # Init addition reduce in dict
+        default = (title, num_views)
 
-        # Get entry if it exists, otherwise set default
-        entry = result.get(key, default)
+        # Check to see if a higher number has been found for given key
+        if key in result:
+            if num_views > result[key][1]:
+                result[key] = (title, num_views)
+        else:
+            result[key] = default
 
-        # Add the number of views together
-        result[key] = (entry[0], entry[1] + num_views)
-
-    # Sort the results in decreasing order
-    result = {k: v for k, v in sorted(result.items(), key=lambda x: x[1][1], reverse=True)}
+    result = {k: v for k, v in sorted(result.items(), key=lambda item: item[1][1], reverse=True)}
 
     # Print data to file
-    # e.g: WikiBooks_Dutch	('Land Ho!', 431)
-    # e.g: WikiVoyage_German ('Hawaii', 339)
     for key, value in result.items():
         my_output_stream.write(key + "\t" + str(value) + "\n")
 
@@ -70,10 +91,48 @@ def my_reduce(my_input_stream, my_output_stream, my_reducer_input_parameters):
 # FUNCTION my_spark_core_model
 # ------------------------------------------
 def my_spark_core_model(sc, my_dataset_dir):
-    pass
+    inputRDD = sc.textFile(my_dataset_dir)
+
+    # Process the raw lines into usable tuples
+    mapped_lines = inputRDD.map(process_line)
+
+    # Map to a usable format
+    mapped_results = mapped_lines.map(lambda line: (line[0] + "_" + line[2], (line[1], line[3])))
+
+    # Find the highest number of views per project and language (key)
+    highest_views = mapped_results.reduceByKey(lambda x1, x2: max(x1, x2, key=lambda x: int(x[1])))
+
+    # Sort the reduced tuples by total run-outs in descending order
+    sorted_tuples = highest_views.sortBy(lambda x: x[1][1], ascending=False)
+
+    # Collect the totals
+    result = sorted_tuples.collect()
+
+    # # Print the results
+    for item in result:
+        print(str(item[0]) + "\t" + str(item[1]))
 
 # ------------------------------------------
 # FUNCTION my_spark_streaming_model
 # ------------------------------------------
 def my_spark_streaming_model(ssc, monitoring_dir):
-    pass
+    # input stream textFileStream
+    input_stream = ssc.textFileStream(monitoring_dir)
+
+    # Process the raw lines into usable tuples
+    mapped_lines = input_stream.map(process_line)
+
+    # Map the processed lines into a usable format
+    usable_map = mapped_lines.map(lambda line: (line[0] + "_" + line[2], (line[1], line[3])))
+
+    # Reduce by key to get the web page with the highest views
+    highest_views = usable_map.reduceByKey(lambda x1, x2: max(x1, x2, key=lambda x: int(x[1])))
+
+    # Transform to sort results in descending order
+    final_stream = highest_views.transform(lambda rdd: rdd.sortBy(lambda item: item[1][1], ascending=False))
+
+    # Persist
+    final_stream.persist(pyspark.StorageLevel.MEMORY_AND_DISK)
+
+    # pprint
+    final_stream.pprint()
