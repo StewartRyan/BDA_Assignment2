ff9e7174abbe415a5bbed93da76e432291173c5f
Ryan Stewart <ryan.stewart@mycit.ie>
2020-05-05 04:06:05 +0100

    Cosmetic update

--- my_python_spark/A02_Part5.py
+++ my_python_spark/A02_Part5.py
@@ -8,7 +8,7 @@ def my_map(my_input_stream, my_output_stream, my_mapper_input_parameters):
         # Process line
         processed_line = process_line(line)
 
-        # Assign all heading values
+        # Assign all delimited values
         wikimedia_project = processed_line[0]
         web_page_name = processed_line[1]
         language = processed_line[2]
@@ -18,10 +18,9 @@ def my_map(my_input_stream, my_output_stream, my_mapper_input_parameters):
         result[web_page_name] = (wikimedia_project, num_views, language)
 
     # Print data to file
-    # e.g: "Man of Tai Chi" ('Wikipedia', 25, 'German')
-    # e.g: "Decision at Sundown" ('WikiVoyage', 5, 'English')
+    # e.g: "Rafael Nadal" ('Wikipedia', 25, 'Spanish')
+    # e.g: "The Netherlands" ('WikiVoyage', 5, 'English')
     for key, value in result.items():
-        print(key + "\t" + str(value))
         my_output_stream.write(key + "\t" + str(value) + "\n")
 
 
@@ -37,17 +36,20 @@ def my_reduce(my_input_stream, my_output_stream, my_reducer_input_parameters):
         # where the value is another tuple: e.g: ('WikiVoyage', 6, 'Dutch')
         processed_entry = get_key_value(item)
 
-        title = processed_entry[0]
+        # Extrapolate web page name
+        web_page_name = processed_entry[0]
         others = processed_entry[1]
 
+        # Extrapolate the relevant delimited values
         wikimedia_project = others[0]
         num_views = int(others[1])
         language = others[2]
 
+        # Make a key by which we want to determine our results
         key = wikimedia_project + '_' + language
 
         # Set a default for if there is no entry for given key
-        default = (title, num_views)
+        default = (web_page_name, num_views)
 
         # Get entry if it exists, otherwise set default
         entry = result.get(key, default)
@@ -56,11 +58,11 @@ def my_reduce(my_input_stream, my_output_stream, my_reducer_input_parameters):
         result[key] = (entry[0], entry[1] + num_views)
 
     # Sort the results in decreasing order
-    result = {k: v for k, v in sorted(result.items(), key=lambda item: item[1][1], reverse=True)}
+    result = {k: v for k, v in sorted(result.items(), key=lambda x: x[1][1], reverse=True)}
 
     # Print data to file
     # e.g: WikiBooks_Dutch	('Land Ho!', 431)
-    # e.g: WikiVoyage_German ('King of Jazz', 339)
+    # e.g: WikiVoyage_German ('Hawaii', 339)
     for key, value in result.items():
         my_output_stream.write(key + "\t" + str(value) + "\n")
 
